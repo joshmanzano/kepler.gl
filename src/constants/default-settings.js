bdd8b614c7a3a12fc76a231b1f702ae46e146119
hkittylover <Hkittylover_320>
2020-01-14 23:56:48 +0800

    test deploy

--- src/constants/default-settings.js
+++ src/constants/default-settings.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Uber Technologies, Inc.
+// Copyright (c) 2020 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -137,6 +137,7 @@ export const DEFAULT_MAP_STYLES = [
   {
     id: 'dark',
     label: 'Dark',
+    // url: 'mapbox://styles/hkittylover/cjxedi6ph06by1dqmtemz12ya',
     url: 'mapbox://styles/uberdata/cjoqbbf6l9k302sl96tyvka09',
     icon: `${ICON_PREFIX}/UBER_DARK_V2.png`,
     layerGroups: DEFAULT_LAYER_GROUPS
